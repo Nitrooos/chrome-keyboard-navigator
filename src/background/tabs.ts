7037458da1b0889e3ed177600cd549813eb197cc
Bartosz Kostaniak <bartosz.kostaniak@cluepoints.com>
2021-09-27 22:36:28 +0200

    KEY-8 Change let -> const

--- src/background/tabs.ts
+++ src/background/tabs.ts
@@ -15,8 +15,8 @@ async function openInRightNextTab(url: string, callback: Callback, options?: Cre
 }
 
 async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
